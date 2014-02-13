6c675034596044b3f83c03e9071460c11312b60a
islamsamak <islamsamak01@gmail.com>
2014-02-13 04:14:31 +0200

    - Drawing direction between two locations "Dr. Mathias SEGUY : http://android2ee.com"

---
+++ src/com/oneteam/framework/android/location/direction/model/GDPoint.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2013 Android2EE (Dr. Mathias SEGUY : http://android2ee.com : http://mathias-seguy.developpez.com)
+ * Copyright (C) 2013 OneTeam (IslamSamak : islamsamak01@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**<ul>
+ * <li>GoogleMapSample</li>
+ * <li>com.android2ee.formation.librairies.google.map.utils.direction.model</li>
+ * <li>13 sept. 2013</li>
+ * 
+ * <li>======================================================</li>
+ *
+ * <li>Projet : Mathias Seguy Project</li>
+ * <li>Produit par MSE.</li>
+ *
+ /**
+ * <ul>
+ * Android Tutorial, An <strong>Android2EE</strong>'s project.</br> 
+ * Produced by <strong>Dr. Mathias SEGUY</strong>.</br>
+ * Delivered by <strong>http://android2ee.com/</strong></br>
+ *  Belongs to <strong>Mathias Seguy</strong></br>
+ ****************************************************************************************************************</br>
+ * This code is free for any usage except training and can't be distribute.</br>
+ * The distribution is reserved to the site <strong>http://android2ee.com</strong>.</br>
+ * The intelectual property belongs to <strong>Mathias Seguy</strong>.</br>
+ * <em>http://mathias-seguy.developpez.com/</em></br> </br>
+ * 
+ * *****************************************************************************************************************</br>
+ *  Ce code est libre de toute utilisation mais n'est pas distribuable.</br>
+ *  Sa distribution est reservée au site <strong>http://android2ee.com</strong>.</br> 
+ *  Sa propriété intellectuelle appartient à <strong>Mathias Seguy</strong>.</br>
+ *  <em>http://mathias-seguy.developpez.com/</em></br> </br>
+ * *****************************************************************************************************************</br>
+ */
+package com.oneteam.framework.android.location.direction.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * @author Mathias Seguy (Android2EE)
+ * @goals
+ *        This class aims to define a GoogleDirection Point which is bound to the JSon structure
+ *        returned by the webService :
+ *        "http://maps.googleapis.com/maps/api/directions/json?" + "origin=" + start.latitude + ","
+ *        + start.longitude + "&destination=" + end.latitude + "," + end.longitude
+ *        + "&sensor=false&units=metric&mode=driving";
+ */
+public class GDPoint {
+	double mLat;
+	double mLng;
+	/**
+	 * The corresponding LatLng
+	 * Not in the JSon Object. It's an helpful attribute
+	 */
+	private LatLng mLatLng = null;
+	
+	/**
+	 * The builder
+	 * @param coordinate retrieve from JSon
+	 */
+	public GDPoint(double lat,double lng) {
+		super();
+		this.mLat = lat;
+		this.mLng=lng;
+	}
+
+	/**
+	 * @return The LatLng Object linked with that point
+	 */
+	public LatLng getLatLng() {
+		if (mLatLng == null) {
+			mLatLng = new LatLng(mLat,mLng);
+		}
+		return mLatLng;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "["+mLat+","+mLng+"]";
+	}
+}
